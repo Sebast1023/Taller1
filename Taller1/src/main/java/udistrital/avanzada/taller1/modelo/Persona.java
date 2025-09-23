607a913c3d95b1e89e772c6d02357af8b6d460f2
ymbermudezt <230941078+ymbermudezt@users.noreply.github.com>
2025-09-23 15:59:21 -0500

    Creado paquete modelo y creado clase en blanco persona

---
+++ Taller1/src/main/java/udistrital/avanzada/taller1/modelo/Persona.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package udistrital.avanzada.taller1.modelo;
+
+/**
+ *
+ * @author mauri
+ */
+public class Persona {
+    
+}
