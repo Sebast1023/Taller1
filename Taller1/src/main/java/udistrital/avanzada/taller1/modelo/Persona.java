235c0b4533d20caef2449632896a25745e15bad1
ymbermudezt <230941078+ymbermudezt@users.noreply.github.com>
2025-09-23 17:58:03 -0500

    Clase base persona

--- Taller1/src/main/java/udistrital/avanzada/taller1/modelo/Persona.java
+++ Taller1/src/main/java/udistrital/avanzada/taller1/modelo/Persona.java
@@ -1,13 +1,95 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package udistrital.avanzada.taller1.modelo;
-
 /**
  *
- * @author mauri
+ * @author mauricio
  */
-public class Persona {
+public abstract class Persona {
+    private int id;
+    protected String nombre;    
+    protected String apellido;
+    protected String cedula;
+    protected String numero;
+    protected String correo;
+    protected String membresia;
+    protected String contraseña;
+
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, String apellido, String cedula, String numero, String correo, String membresia, String contraseña) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.numero = numero;
+        this.correo = correo;
+        this.membresia = membresia;
+        this.contraseña = contraseña;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getMembresia() {
+        return membresia;
+    }
+
+    public void setMembresia(String membresia) {
+        this.membresia = membresia;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
     
 }
