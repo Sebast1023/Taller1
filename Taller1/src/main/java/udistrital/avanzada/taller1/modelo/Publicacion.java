cc63a823fba1ef4fa637887de143eaf312bdb300
ymbermudezt <230941078+ymbermudezt@users.noreply.github.com>
2025-09-23 23:37:41 -0500

    agregada referencia atributo de creador a Publicacion.java

--- Taller1/src/main/java/udistrital/avanzada/taller1/modelo/Publicacion.java
+++ Taller1/src/main/java/udistrital/avanzada/taller1/modelo/Publicacion.java
@@ -8,17 +8,19 @@ package udistrital.avanzada.taller1.modelo;
  */
 public class Publicacion {
     String titulo;
-    String Contenido;    
+    String Contenido;
     int likes;
+    Persona creador;
 
     public Publicacion() {
         this.likes = 0;
     }
     
-    public Publicacion(String descripocion, String titulo) {
+    public Publicacion(String descripocion, String titulo, Persona creador) {
         this.titulo = titulo;
         this.Contenido = descripocion;
         this.likes = 0;
+        this.creador = creador;
     }
 
     public String getContenido() {
@@ -44,6 +46,13 @@ public class Publicacion {
     public void setLikes(int likes) {
         this.likes = likes;
     }
-    
+
+    public Persona getCreador() {
+        return creador;
+    }
+
+    public void setCreador(Persona creador) {
+        this.creador = creador;
+    }
     
 }
