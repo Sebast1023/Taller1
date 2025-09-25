200e6ce9b375f4566f66adb60c885aa9479f38ac
zapakevin <kjzapal@udistrital.edu.co>
2025-09-24 21:21:17 -0500

    se crea clase vehiculo y sus hijas moto y patineta

---
+++ Taller1/src/main/java/udistrital/avanzada/taller1/modelo/Moto.java
@@ -0,0 +1,20 @@
+package udistrital.avanzada.taller1.modelo;
+
+public class Moto extends Vehiculo {
+
+    public Moto(String marca, String modelo, int ano, int potencia, int carga, String color, String placa) {
+        super(marca, modelo, ano, potencia, carga, color, placa);
+    }
+
+    @Override
+    public String toString() {
+        return "Moto{" +
+                "marca='" + getMarca() + '\'' +
+                ", modelo='" + getModelo() + '\'' +
+                ", ano=" + getAno() +
+                ", color='" + getColor() + '\'' +
+                ", placa='" + getPlaca() + '\'' +
+                ", potencia=" + getPotencia() +
+                '}';
+    }
+}
